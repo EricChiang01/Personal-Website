409220e4d16c54e3da462e241d31e19402e4fc0f
EricChiang01 <EricChiang365@gmail.com>
2023-03-26 00:31:33 -0700

    changes

--- script.js
+++ script.js
@@ -2,3 +2,21 @@ function toggleMode() {
   var element = document.body;
   element.classList.toggle("dark-mode");
 }
+
+// Function to toggle dropdown menu
+function toggleDropdown(event) {
+  const dropdownContent = event.target.nextElementSibling;
+  dropdownContent.classList.toggle('show');
+  }
+  
+  // Close the dropdown if the user clicks outside of it
+  window.onclick = function(event) {
+  if (!event.target.matches('.dropdown-btn')) {
+  const dropdowns = document.querySelectorAll('.dropdown-content');
+  dropdowns.forEach(dropdown => {
+  if (dropdown.classList.contains('show')) {
+  dropdown.classList.remove('show');
+  }
+  });
+  }
+  }
