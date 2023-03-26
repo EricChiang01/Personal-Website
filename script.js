2b3f67585661f8a22d5d1bc9d9c3e8e3b65f16ad
EricChiang01 <EricChiang365@gmail.com>
2023-03-26 00:21:14 -0700

    changes

--- script.js
+++ script.js
@@ -1,18 +1,4 @@
 function toggleMode() {
-    var element = document.body;
-    element.classList.toggle("dark-mode");
-  }
-
-// Get all the project title elements
-const projectTitles = document.querySelectorAll('#projects li h3');
-
-// Loop through each project title
-projectTitles.forEach(title => {
-  // Add a click event listener to each project title
-  title.addEventListener('click', () => {
-    // Get the sibling <p> element that contains the project description
-    const description = title.nextElementSibling;
-    // Toggle the visibility of the description element
-    description.classList.toggle('show');
-  });
-});
+  var element = document.body;
+  element.classList.toggle("dark-mode");
+}
